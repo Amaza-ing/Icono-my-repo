622de0c49eaa9bdc14c20a1586b5ae675b8a7bee
Amaza-ing <52103323+Amaza-ing@users.noreply.github.com>
2026-04-14 11:47:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ console.log("More changes");
 // esto es otro cambio
 
 console.log("Se ha añadido la feature A");
-
 console.log("Se ha completado la feature B definitivamente");
+
+console.log("otro desarrollo de un compañero");
 
 console.log("Otra funcionalidad de un compañero");
 
