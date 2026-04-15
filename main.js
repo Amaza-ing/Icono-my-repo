5f727f5ab22ebeb52246871f8050f78d1781cef5
Adrián Maza <adri_mv1@hotmail.com>
2026-04-15 09:16:22 +0200

    format document main.js

--- main.js
+++ main.js
@@ -7,17 +7,16 @@ console.log("Se ha añadido la feature A");
 console.log("Se ha completado la feature B definitivamente");
 console.log("¡¡¡¡Se ha creado la funcionalidad C!!!!");
 
-
 console.log("otro desarrollo de un compañero");
 
-console.log("Otra funcionalidad de un compañero");
+console.log("asdfasdf Otra funcionalidad de un compañero");
 
-function menu(){
-  console.log("Soy el menú");  
+function menu() {
+  console.log("Soy el menú");
 }
 
 function camera() {
-  console.log("Usando la cámara");  
+  console.log("Usando la cámara");
 }
 
 console.log("goodbye");
