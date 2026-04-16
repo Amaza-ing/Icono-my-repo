5f8aaa51530fa8142f077ce7f97ae8a14b52fa19
Adrián Maza <adri_mv1@hotmail.com>
2026-04-16 11:53:04 +0200

    feat: asdfasdfasdf

--- main.js
+++ main.js
@@ -1,25 +1,10 @@
-console.log("Hi!!!!");
-
-console.log("More changes");
-
-console.log("Se ha añadido la feature A");
-console.log("Se ha completado la feature B definitivamente");
-console.log("¡¡¡¡Se ha creado la funcionalidad C!!!!");
-
-console.log("otro desarrollo de un compañero");
-
-console.log("asdfasdf Otra funcionalidad de un compañero");
-
 function menu() {
-  console.log("Soy el menú");
 }
 
 function camera() {
-  console.log("Usando la cámara!");
 }
 
 function login() {
-  console.log("Login");  
 }
 
 function ad() {}
